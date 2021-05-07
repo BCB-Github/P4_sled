7366a09a21e60f97b29e40f11570b33881d2cd6a
Benjamin Christopher Barlous <barlousb@gmail.com>
2021-05-07 09:10:20 +0200

    ZXCUVBLÆLIKFHYXGF

--- nonlinear_med_loader/main.m
+++ nonlinear_med_loader/main.m
@@ -1,9 +1,12 @@
 
-clear
+%clear
 %Simulation parameters go here
 T_sample = 0.001;           %Sample time of controller
 step_size_array = [1, 0.1];              %% magnitude of step response
 disc_type = 0;              %% 0 is for Backwards Difference, 1 is for forwards_difference
+
+%antiwindup  % either 'on' or 'off'
+anti_windup = 'on'
 
 
 
@@ -23,9 +26,6 @@ sat_cont = 1;               %% CHOICE OF CONTROLLER
 CONT_PI_LEAD_ARRAY = [1, 2];
 CONT_LEAD_ARRAY  = 0;
 
-
-%antiwindup  % either 'on' or 'off'
-anti_windup = 'on'
 
 
 
@@ -72,81 +72,80 @@ for j = 1:length(step_size_array)
 end
 
 
-
-%%PID SIMULATION - antiwindup type 2
-
-assign_blocks('on1', 'PI-LEAD')
-for j = 1:length(step_size_array)
-    step_size = step_size_array(j);
-        for i = 1:length(CONT_PI_LEAD_ARRAY)
-            disp("Controller number " + i)
-            
-            %% if the simulation has run with the same parameters, then don't run it again
-            if isequal(PI_LEAD_ARRAY_old_1(i +(j-1)*length(CONT_PI_LEAD_ARRAY), :), [T_sample, step_size_array(j), CONT_PI_LEAD_ARRAY(i)])
-                disp("This controller has already been simulated")
-                continue
-            end
-            
-            %Timer
-            tic
-            sat_cont = CONT_PI_LEAD_ARRAY(i);
-            run("NonLinearModelParameters");
-            toc
-            out = sim("Discrete_Model");
-            
-            PI_LEAD_ARRAY_RESULTS_1(:, i + (j-1) * length(CONT_PI_LEAD_ARRAY)) = out.last_test;
-            toc
-            
-            %% Save values of the current run simulation
-            PI_LEAD_ARRAY_old_1(i + (j-1) * length(CONT_PI_LEAD_ARRAY), :) = [T_sample, step_size_array(j), CONT_PI_LEAD_ARRAY(i)];
-        end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-disp("This is the LEAD Simulation")
-
-%% for LEAD
-assign_blocks(anti_windup, 'LEAD')
-for j = 1:length(step_size_array)
-    if CONT_LEAD_ARRAY(1) == 0
-    break
-    end
-    step_size = step_size_array(j);
-        for i = 1:length(CONT_LEAD_ARRAY)
-            disp("Controller number " + i)
-            
-            %% if the simulation has run with the same parameters, then don't run it again
-            if isequal(CONT_LEAD_ARRAY_OLD(i +(j-1)*length(CONT_LEAD_ARRAY), :), [T_sample, step_size_array(j), CONT_LEAD_ARRAY(i)])
-                disp("This controller has already been simulated")
-                continue
-            end
-            
-            %Timer
-            tic
-            sat_cont = CONT_LEAD_ARRAY(i);
-            run("NonLinearModelParameters");
-            toc
-            out = sim("Discrete_Model");
-            
-            CONT_LEAD_ARRAY_RESULTS(:, i + (j-1) * length(CONT_LEAD_ARRAY)) = out.last_test;
-            toc
-            
-            %% Save values of the current run simulation
-            CONT_LEAD_ARRAY_OLD(i + (j-1) * length(CONT_LEAD_ARRAY), :) = [T_sample, step_size_array(j), CONT_LEAD_ARRAY(i)];
-
-        end
-end
-
+%% VIRKER IKKE LIGE NU
+% %%PID SIMULATION - antiwindup type 2
+% assign_blocks('on1', 'PI-LEAD')
+% for j = 1:length(step_size_array)
+%     step_size = step_size_array(j);
+%         for i = 1:length(CONT_PI_LEAD_ARRAY)
+%             disp("Controller number " + CONT_PI_LEAD_ARRAY(i))
+%             
+%             %% if the simulation has run with the same parameters, then don't run it again
+%             if isequal(PI_LEAD_ARRAY_old_1(i +(j-1)*length(CONT_PI_LEAD_ARRAY), :), [T_sample, step_size_array(j), CONT_PI_LEAD_ARRAY(i)])
+%                 disp("This controller has already been simulated")
+%                 continue
+%             end
+%             
+%             %Timer
+%             tic
+%             sat_cont = CONT_PI_LEAD_ARRAY(i);
+%             run("NonLinearModelParameters");
+%             toc
+%             out = sim("Discrete_Model");
+%             
+%             PI_LEAD_ARRAY_RESULTS_1(:, i + (j-1) * length(CONT_PI_LEAD_ARRAY)) = out.last_test;
+%             toc
+%             
+%             %% Save values of the current run simulation
+%             PI_LEAD_ARRAY_old_1(i + (j-1) * length(CONT_PI_LEAD_ARRAY), :) = [T_sample, step_size_array(j), CONT_PI_LEAD_ARRAY(i)];
+%         end
+% end
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% disp("This is the LEAD Simulation")
+% 
+% %% for LEAD
+% assign_blocks(anti_windup, 'LEAD')
+% for j = 1:length(step_size_array)
+%     if CONT_LEAD_ARRAY(1) == 0
+%     break
+%     end
+%     step_size = step_size_array(j);
+%         for i = 1:length(CONT_LEAD_ARRAY)
+%             disp("Controller number " + CONT_LEAD_ARRAY(i))
+%             
+%             %% if the simulation has run with the same parameters, then don't run it again
+%             if isequal(CONT_LEAD_ARRAY_OLD(i +(j-1)*length(CONT_LEAD_ARRAY), :), [T_sample, step_size_array(j), CONT_LEAD_ARRAY(i)])
+%                 disp("This controller has already been simulated")
+%                 continue
+%             end
+%             
+%             %Timer
+%             tic
+%             sat_cont = CONT_LEAD_ARRAY(i);
+%             run("NonLinearModelParameters");
+%             toc
+%             out = sim("Discrete_Model");
+%             
+%             CONT_LEAD_ARRAY_RESULTS(:, i + (j-1) * length(CONT_LEAD_ARRAY)) = out.last_test;
+%             toc
+%             
+%             %% Save values of the current run simulation
+%             CONT_LEAD_ARRAY_OLD(i + (j-1) * length(CONT_LEAD_ARRAY), :) = [T_sample, step_size_array(j), CONT_LEAD_ARRAY(i)];
+% 
+%         end
+% end
+% 
 
 
 
@@ -161,7 +160,7 @@ save_system
 
 
 
-if PI_LEAD_array ~= 0
+if CONT_PI_LEAD_ARRAY ~= 0
     plot(time, PI_LEAD_ARRAY_RESULTS)
     %plot(time, PI_LEAD_ARRAY_RESULTS_1)
 end
